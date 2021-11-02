7e9d00b40e164a31c2dd0453dcbcf3bb7a72edf6
sillyPhotons <ruihengsu@gmail.com>
2021-11-02 15:08:23 -0700

    Great

--- Klayout/salt/Ray/pymacros/RayDevelopment.py.py
+++ Klayout/salt/Ray/pymacros/RayDevelopment.py.py
@@ -1,51 +1,86 @@
-import pya 
-
-import pya
-import numpy as np
 import pya
 import numpy as np
 
-
-class LabelArray_VariablePitch(pya.PCellDeclarationHelper):
+class CrossRingLabeled(pya.PCellDeclarationHelper):
     """
-    The PCell declaration for the LabelArray_VariablePitch
+    The PCell declaration for the CrossRingLabeled
     """
 
     def __init__(self):
+
         # Important: initialize the super class
-        super(LabelArray_VariablePitch, self).__init__()
+        super(CrossRingLabeled, self).__init__()
 
         # declare the parameters
         self.param("l", self.TypeLayer, "Layer", default=pya.LayerInfo(1, 0))
-        self.param("horizontal", self.TypeBoolean,
-                   "Horizontal labels?", default=True)
-        self.param("labels", self.TypeList,
-                   "List of labels", default=["1", "2", "3"])           
-        self.param("text_pitch", self.TypeList,
-                   "Distance between adjacent Labels", default = np.array([0, 50, 100], dtype=int).tolist())
+
+        self.param("cwidth", self.TypeDouble, "Cross width", default=120.0)
+        self.param("clength", self.TypeDouble, "Cross length", default=120.0)
+        self.param("cthick", self.TypeDouble, "Cross thickness", default=8.0)
+
+        self.param("rows", self.TypeInt, "Number of rows", default=2)
+        self.param("columns", self.TypeInt, "Number of columns", default=2)
+
+        self.param("row_step", self.TypeDouble, "Row step", default=300.0)
+        self.param("col_step", self.TypeDouble, "Col step", default=300.0)
+
+        self.param("width", self.TypeDouble,
+                   "Text x-Position Parameter", default=180.0)
+
+        self.param("text_distance", self.TypeDouble,
+                   "Text distance", default=75.0)
+
         self.param("text_scale", self.TypeDouble,
-                   "Text scaling factor", default=10)
+                   "Text scaling factor", default=0.04)
 
     def display_text_impl(self):
         # Provide a descriptive text for the cell
-        return "LabelArray_VariablePitch(Layer=%s)" % (str(self.l))
+        return "CrossRingLabeled(L=%s)" % (str(self.l))
 
     def produce_impl(self):
-      
-        print(self.text_pitch)
 
         scaling_factor = int(1/self.layout.dbu)
-        for i, lab in enumerate(self.labels):
-            lab = lab.strip()
-            label = pya.TextGenerator.default_generator().text(
-                "{}".format(lab), self.layout.dbu/self.text_scale)
+        print(scaling_factor)
+        x = np.linspace(0, self.rows, endpoint=False,
+                        num=self.rows, dtype=int)*self.row_step
+        y = np.linspace(0, self.columns, endpoint=False,
+                        num=self.columns,)*self.col_step
 
-            if self.horizontal:
-                label = label.move(i*scaling_factor*float(self.text_pitch[i]), 0)
-            else:
-                label = label.move(0, -i*scaling_factor*float(self.text_pitch[i]))
+        XX, YY = np.meshgrid(x, y, indexing='ij')
 
-            self.cell.shapes(self.l_layer).insert(label)
+        grid = np.zeros((self.rows, self.columns), dtype='i,i')
+
+        for i in range(0, self.rows):
+            for j in range(0, self.columns):
+                grid[i, j] = (XX[i, j], YY[i, j])
+                
+
+        for row_col in [grid[0, :], grid[-1, :], grid[:, 0], grid[:, -1]]:
+            for p in row_col.flatten():
+                print(p)
+                c_x = float(p[0])
+                c_y = float(p[1])
+                c1 = pya.DPoint(c_x-self.cwidth/2.0, c_y)*scaling_factor
+                c2 = pya.DPoint(c_x+self.cwidth/2, c_y)*scaling_factor
+                p1 = pya.DPath([c1, c2], self.cthick*scaling_factor)
+
+                l1 = pya.Region()
+                l1.insert(p1)
+
+                c3 = pya.DPoint(c_x, c_y-self.clength/2)*scaling_factor
+                c4 = pya.DPoint(c_x, c_y+self.clength/2)*scaling_factor
+                p2 = pya.DPath([c3, c4], self.cthick*scaling_factor)
+
+                l2 = pya.Region()
+                l2.insert(p2)
+
+                label = pya.TextGenerator.default_generator().text("{},{}".format(int(c_x-x.max()/2),
+                                                                                  int(c_x-y.max()/2)),
+                                                                   self.text_scale*self.layout.dbu).move(1000*(c_x-self.width/2 + self.width/40),
+                                                                                                         1000*c_y - 1000*self.text_distance)
+
+            result = l1 + l2 + label
+            self.cell.shapes(self.l_layer).insert(result)
 
 
 class RayDev(pya.Library):
@@ -59,12 +94,14 @@ class RayDev(pya.Library):
         self.description = "Ray De's PCell Library"
 
         # Create the PCell declarations
-        
+
         # self.layout().register_pcell("OverlayAlignMarkArray", OverlayAlignMarkArray())
-        
-        self.layout().register_pcell("LabelArray_VariablePitch", LabelArray_VariablePitch())
+
+        self.layout().register_pcell("SingleOhmicContact", SingleOhmicContact())
+        self.layout().register_pcell("CrossRingLabeled", CrossRingLabeled())
         # Register us with the name "SerpentineLib".
         self.register("RayDev")
+
 
 # Instantiate and register the library
 RayDev()
--- Klayout/salt/Ray/python/SingleOhmicContact.py
+++ Klayout/salt/Ray/python/SingleOhmicContact.py
@@ -18,6 +18,11 @@ class SingleOhmicContact (pya.PCellDeclarationHelper):
         self.param("A", self.TypeInt, "Amplitude", default=1)
         self.param("H", self.TypeInt, "Harmonic", default=1)
         self.param("R", self.TypeInt, "Resolution", default=2)
+        
+        self.param("oxp", self.TypeBoolean, "Enable +x meander", default=True)
+        self.param("oxn", self.TypeBoolean, "Enable -x meander", default=True)
+        self.param("oyp", self.TypeBoolean, "Enable +y meander", default=True)
+        self.param("oyn", self.TypeBoolean, "Enable -y meander", default=True)
 
     def display_text_impl(self):
         # Provide a descriptive text for the cell
@@ -26,41 +31,46 @@ class SingleOhmicContact (pya.PCellDeclarationHelper):
     def produce_impl(self):
         # This is the main part of the implementation: create the layout
         pts = []
-
-        edges_x = np.linspace(-1, 1, num=self.R, endpoint=True)
-        edges_y = self.A*np.sin(self.H*2*np.pi*edges_x)
-
-        print(edges_x)
-        print(edges_y)
-
-        edges_x1 = edges_x*self.X/2
-        edges_y1 = -1*edges_y - self.Y/2
-        print(edges_x1)
-        print(edges_y1)
-
+        
+        ref_x = np.linspace(-1, 1, num=self.R, endpoint=True)
+        short_x = np.linspace(-1, 1, num=2, endpoint=True)
+        
+        edges_x1 = ref_x*self.X/2
+        edges_y1 = -1*self.A*np.sin(self.H*2*np.pi*ref_x) - self.Y/2
+        
+        if not self.oyn:
+          edges_x1 = short_x*self.X/2
+          edges_y1 = -1*self.A*np.sin(self.H*2*np.pi*short_x) - self.Y/2
+        
         for p in zip(edges_x1, edges_y1):
             pts.append(pya.DPoint(p[0], p[1]))
 
-        edges_x2 = edges_y + self.X/2
-        edges_y2 = edges_x*self.Y/2
-        print(edges_x2)
-        print(edges_y2)
+        edges_x2 = self.A*np.sin(self.H*2*np.pi*ref_x) + self.X/2
+        edges_y2 = ref_x*self.Y/2
+        
+        if not self.oxp:
+          edges_x2 = self.A*np.sin(self.H*2*np.pi*short_x) + self.X/2
+          edges_y2 = short_x*self.Y/2
 
         for p in zip(edges_x2, edges_y2):
             pts.append(pya.DPoint(p[0], p[1]))
 
-        edges_x3 = -1*edges_x*self.X/2
-        edges_y3 = edges_y + self.Y/2
-        print(edges_x3)
-        print(edges_y3)
+        edges_x3 = -1*ref_x*self.X/2
+        edges_y3 = self.A*np.sin(self.H*2*np.pi*ref_x) + self.Y/2
+        
+        if not self.oyp:
+          edges_x3 = -1*short_x*self.X/2
+          edges_y3 = self.A*np.sin(self.H*2*np.pi*short_x) + self.Y/2
 
         for p in zip(edges_x3, edges_y3):
             pts.append(pya.DPoint(p[0], p[1]))
 
-        edges_x4 = -1*edges_y - self.X/2
-        edges_y4 = -1*edges_x*self.Y/2
-        print(edges_x4)
-        print(edges_y4)
+        edges_x4 = -1*self.A*np.sin(self.H*2*np.pi*ref_x) - self.X/2
+        edges_y4 = -1*ref_x*self.Y/2
+
+        if not self.oxn:
+          edges_x4 = -1*self.A*np.sin(self.H*2*np.pi*short_x) - self.X/2
+          edges_y4 = -1*short_x*self.Y/2
 
         for p in zip(edges_x4, edges_y4):
             pts.append(pya.DPoint(p[0], p[1]))
